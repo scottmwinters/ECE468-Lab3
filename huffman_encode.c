eac24629c8c2c740786c77f35837751fd3d680f8
Taylor Steinberg <tdstein@g.clemson.edu>
2014-02-20 12:33:17 -0500

    Sorted Linked List Insert Implementation

--- huffman_encode.c
+++ huffman_encode.c
@@ -6,6 +6,60 @@
 #define INPUT_TYPE unsigned char
 #define MAX_STRING_LEN 255
 
+typedef struct entry_tag {
+	unsigned char val;
+	int freq;
+	struct entry_tag *fwd;
+} entry;
+
+typedef struct node_tag {
+	entry *entry; 
+	struct node_tag *left;
+	struct node_tag *right;
+} node;
+
+entry *head = NULL;
+
+// Sorted Insert
+void create_entry(unsigned char val) {
+
+	entry *current = calloc(sizeof(*current), 1);
+
+	current->val = val;
+	current->freq = 1;
+	current->fwd = NULL;
+
+	if(head == NULL) {
+		head = current;
+	} else {
+
+		entry *rover = head;
+
+		if(val < head->val) {
+			current->fwd = head;
+			head = current;
+		} else {
+
+			entry *previous = rover;
+			rover = rover->fwd;
+
+			while(rover != NULL) {
+		
+				if(val > previous->val && val < rover->val) {
+					current->fwd = rover;
+					previous->fwd = current;
+					break;
+				}
+
+				previous = rover;
+				rover = rover->fwd;
+			}
+
+			if(rover == NULL) previous->fwd = current;
+		}
+	}
+}
+
 
 int main(int argc, char **argv) {
 
@@ -27,24 +81,28 @@ int main(int argc, char **argv) {
 	assert(fpIn != NULL);
 	assert(fpOut != NULL);
 
+	unsigned char val;
 
-	unsigned int *frequencyArray = NULL;
-	frequencyArray = calloc(sizeof(*frequencyArray), 256);
-	INPUT_TYPE *chr = calloc(sizeof(*chr), 1);
+	while(fread(&val, sizeof(val), 1, fpIn) == 1) {
 
-	while(fread(chr, sizeof(*chr), 1, fpIn) == 1) frequencyArray[*chr]++;
-
-	rewind(fpIn);
-
-	for(int i = 0; i < 256; i++) {
-		unsigned char x = frequencyArray[i];
-		int j = i;
-		while(j > 0 && frequencyArray[j-1] > x) {
-			frequencyArray[j] = frequencyArray[j-1];
-			j--;
+		entry *rover = head;
+		while(rover != NULL) {
+			if(rover->val == val) {
+				rover->freq++;
+				break;
+			} 
+			rover = rover->fwd;
 		}
-		frequencyArray[j] = x;
-	}	
+
+		if(rover == NULL) create_entry(val);
+
+	}
+
+	entry *rover = head;
+	while(rover != NULL) {
+		printf("%d : %d\n", rover->val, rover->freq);
+		rover = rover->fwd;
+	}
 
 	fclose(fpIn);
 	fclose(fpOut);
